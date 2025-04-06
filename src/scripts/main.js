33f9b5b5441c2038d17f9f91c0025a88d47351eb
sirserg <sirserg05@gmail.com>
2025-04-06 19:57:12 +0300

    feat: add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,5 +1,32 @@
 'use strict';
 
+const wall = document.querySelector('.wall');
+const spider = document.querySelector('.spider');
+
 document.addEventListener('click', (e) => {
-  // write code here
+  const clientX = e.clientX;
+  const clientY = e.clientY;
+
+  const wallCoords = wall.getBoundingClientRect();
+  const wallBorder = wall.clientLeft;
+  const spiderCoords = spider.getBoundingClientRect();
+  const spiderDim = spiderCoords.width;
+
+  if (
+    clientY < wallCoords.top ||
+    clientY > wallCoords.bottom ||
+    clientX < wallCoords.left ||
+    clientY > wallCoords.right
+  ) {
+    return;
+  }
+
+  const spiderTop = clientY - wallCoords.top - wallBorder - spiderDim / 2;
+  const spiderLeft = clientX - wallCoords.left - wallBorder - spiderDim / 2;
+
+  const maxSpiderTop = wallCoords.height - wallBorder * 2 - spiderDim;
+  const maxSpiderLeft = wallCoords.width - wallBorder * 2 - spiderDim;
+
+  spider.style.top = `${Math.max(0, Math.min(spiderTop, maxSpiderTop))}px`;
+  spider.style.left = `${Math.max(0, Math.min(spiderLeft, maxSpiderLeft))}px`;
 });
